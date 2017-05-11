a85546b40513e21bbcac07fac9fd8b1b15fdaf96
Dave Baskin <dfbaskin@gmail.com>
2017-05-11 12:54:59 -0400

    Updating App component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>React Mouse/Touch Events Demo</h2>
-        </div>
-        <p className="App-intro">
-          ... to do
-        </p>
-      </div>
-    );
-  }
+export class App extends PureComponent {
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    <h2>React Mouse/Touch Events Demo</h2>
+                </div>
+                <div>
+                    ... to do
+                </div>
+            </div>
+        );
+    }
 }
-
-export default App;
