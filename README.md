3e945b93affc86287f0e1a7eaa05b426f68fd2c9
Dave Baskin <dfbaskin@gmail.com>
2017-05-11 12:40:12 -0400

    Initial files.

---
+++ README.md
@@ -0,0 +1,3 @@
+Demonstration of mouse and touch events in React.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>React Mouse/Touch Events Demo</h2>
+        </div>
+        <p className="App-intro">
+          ... to do
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
