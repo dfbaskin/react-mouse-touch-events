02c901a68b3cc2799d1d02747288adeeda626044
Dave Baskin <dfbaskin@gmail.com>
2019-05-05 22:45:29 -0400

    Updates

---
+++ precache-manifest.021d74f859fdb4fe0ff5c82d4b2d85e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec93e95d888ace4b515ce98cba9a8888",
+    "url": "/react-mouse-touch-events/index.html"
+  },
+  {
+    "revision": "c31aa48f18d020be47fb",
+    "url": "/react-mouse-touch-events/static/css/main.80fd2c26.chunk.css"
+  },
+  {
+    "revision": "92e3e99435d4e7e6107c",
+    "url": "/react-mouse-touch-events/static/js/2.4a5eba98.chunk.js"
+  },
+  {
+    "revision": "c31aa48f18d020be47fb",
+    "url": "/react-mouse-touch-events/static/js/main.58b0f48f.chunk.js"
+  },
+  {
+    "revision": "6763e0c79c261210eefd",
+    "url": "/react-mouse-touch-events/static/js/runtime~main.60c63904.js"
+  }
+]);
